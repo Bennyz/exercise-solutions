5753c31a0a280702f82cfd9dff5f5f42d1aced1c
Benny Zlotnik <benny@z.io>
2015-03-10 21:31:06 +0200

    tidying up

--- text-align-justify.js
+++ text-align-justify.js
@@ -1,14 +1,17 @@
 var justifyLine = function(line, requiredSpaces) {
 
+	// No need to justify if there's just one line
 	if (line.length == 1) {
 		return line;
 	}
 
+	// No need to add spaces after last word, thus cutting out the last word
 	var words = line.slice(0, line.length - 1);
 	var numberOfSpacePositions = words.length;
 	var spacePerPosition = Math.floor(requiredSpaces / numberOfSpacePositions);
 	var remainingSpaces = requiredSpaces % numberOfSpacePositions;
 
+	// Initialize spaces
 	var wordsObject = [];
 	words.forEach(function(word){
 		wordsObject.push({spaces: []});
@@ -53,7 +56,7 @@ var justify = function(str, len) {
 			currLineLength += word.length + 1;
 			currLine.push(word);
 		} else {
-			var requiredSpaces = len - (totalWordLength);
+			var requiredSpaces = len - totalWordLength;
 			lines.push(justifyLine(currLine, requiredSpaces));
 			currLine = [];
 			currLine.push(word);
@@ -65,7 +68,3 @@ var justify = function(str, len) {
 	lines.push(currLine.join(' '));
 	return lines.join('\n');
 };
-//var str = 'Benny wne go for away die new baby new majit';
-var str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.';
-
-console.log(justify(str, 30));
