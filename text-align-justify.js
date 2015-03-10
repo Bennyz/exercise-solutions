57cd49954227eae015edf2c0426ebae2b1b21e23
Benny Zlotnik <benny@z.io>
2015-03-10 18:54:42 +0200

    change method name

--- text-align-justify.js
+++ text-align-justify.js
@@ -1,50 +1,66 @@
-var addSpaces = function(line, requiredSpaces) {
+var justifyLine = function(line, requiredSpaces) {
 
 	if (line.length == 1) {
 		return line;
 	}
 
 	var words = line.slice(0, line.length -1);
-	var spacePositions = word.length - 1;
+	var numberOfSpacePositions = words.length;
+	var spacePerPosition = Math.floor(requiredSpaces / numberOfSpacePositions);
+	var remainingSpaces = requiredSpaces % numberOfSpacePositions;
 
+	var wordsObject = [];
+	words.forEach(function(word){
+		wordsObject.push({spaces: []});
+	});
+
+	// Distribute first round of spaces
+	wordsObject.forEach(function(o) {
+		for (var i = 0; i < spacePerPosition; i++) {
+			o.spaces.push(' ');
+		}
+	});
+
+	// Distribute second round of spaces to avoid gaps
+	if (remainingSpaces > 0) {
+		for (var i = 0; i < remainingSpaces; i++) {
+			wordsObject[i].spaces.push(' ');
+		}
+	}
+
+	// Add spaces to create the justified line
+	var justifiedLine = [];
+	words.forEach(function(word, index) {
+		justifiedLine.push(word);
+		justifiedLine.push(wordsObject[index].spaces.join(''));
+	});
+
+	justifiedLine.push(line[words.length]);
+
+	return justifiedLine.join('');
 }
 
 var justify = function(str, len) {
 	var words = str.split(' ');
 	var lines = [];
 
-	var numberOfWords = words.length;
-	for (var i = 0; i < numberOfWords; i++) {
-		var line = [];
-		var totalLineLength = 0;
-		var currWordLength = 0;
-		var wordCount = 0;
-
-		for (var j = i; j < numberOfWords; j++) {
-			if (words[j].length + totalLineLength < len) {
-				var currWord = words[j];
-				line.push(currWord);
-				currWordLength += currWord.length;
-
-				// Add space to each word
-				totalLineLength += currWord.length + 1;
-				wordCount++;
-			} else {
-
-				// Skip all added words
-				i += wordCount - 1;
-				break;
-			}
-		}
-		if (i + 1 == numberOfWords) {
-			lines.push(line.join(''));
+	var currLine = [];
+	var currLineLength = 0;
+	
+	words.forEach(function(word, index) {
+		if (currLineLength + word.length + 1 < len) {
+			currLineLength += word.length + 1;
+			currLine.push(word);
 		} else {
-			line = addSpaces(line, len - currWordLength);
-			lines.push(line.join(''));
+			var requiredSpaces = len - (currLineLength - currLine.length);
+			lines.push(justifyLine(currLine, requiredSpaces));
+			currLine = [];
+			currLine.push(word);
+			currLineLength = word.length + 1;
 		}
+	});
 
-	}
-
+	lines.push(currLine.join(' '));
 	return lines.join('\n');
 };
 //var str = 'Benny wne go for away die new baby new majit';
